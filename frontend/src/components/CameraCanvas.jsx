b907c97506734b7a0c450127d3fb874e2b5f1fa5
Aref Malek <malek.aref1@gmail.com>
2023-02-12 00:02:39 -0500

    splitting the functions into different components

--- frontend/src/components/CameraCanvas.jsx
+++ frontend/src/components/CameraCanvas.jsx
@@ -5,149 +5,20 @@ import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands";
 
 import { useEffect, useRef } from "react";
 
-const WIDTH = 1280;
-const HEIGHT = 720;
+import {
+  fixedQueue,
+  paletteAlpha,
+  drawLine,
+  isOpen, 
+  toScreen,
+  gesture,
+  distance,
+  distance2d,
+  strokeSize,
+  WIDTH, HEIGHT,
+  palette
+} from "./draw";
 
-const MAX_STROKE_WEIGHT = 10;
-
-// pallette object for colors: one for hand, eraser, fill of circle
-const palette = {
-  red: "#f07167",
-  yellow: "#ffba49",
-  seaGreen: "#20a39e",
-  yellowGreen: "#9fd356",
-  hunterGreen: "#2e5339",
-};
-
-const alpha = "7f";
-// create another palette with alpha appended
-const paletteAlpha = {
-  red: palette.red + alpha,
-  yellow: palette.yellow + alpha,
-  seaGreen: palette.seaGreen + alpha,
-  yellowGreen: palette.yellowGreen + alpha,
-  hunterGreen: palette.hunterGreen + alpha,
-};
-
-class fixedQueue {
-  constructor(size) {
-    this.size = size;
-    this.queue = [];
-  }
-
-  push(item) {
-    if (this.queue.length >= this.size) {
-      this.queue.shift();
-    }
-    this.queue.push(item);
-  }
-
-  get() {
-    return this.queue;
-  }
-
-  average() {
-    let sum = 0;
-    for (let i = 0; i < this.queue.length; i++) {
-      sum += this.queue[i];
-    }
-    return sum / this.queue.length;
-  }
-
-  clear() {
-    this.queue = [];
-  }
-}
-
-const toScreen = (position) => {
-  return {
-    x: position.x * WIDTH,
-    y: position.y * HEIGHT,
-  };
-};
-
-// Dot product of vectors a and b
-const dot = (a, b) => a.x * b.x + a.y * b.y + a.z * b.z;
-
-// Distance between vectors a and b
-const distance = (a, b) => {
-  let x = a.x - b.x;
-  let y = a.y - b.y;
-  let z = a.z - b.z;
-  return Math.sqrt(x * x + y * y + z * z);
-};
-
-const distance2d = (a, b) => {
-  let x = a.x - b.x;
-  let y = a.y - b.y;
-  return Math.sqrt(x * x + y * y);
-};
-
-// Add two vectors a and b
-const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y, z: a.z + b.z });
-
-// Subtract vector b from vector a
-const sub = (a, b) => ({ x: a.x - b.x, y: a.y - b.y, z: a.z - b.z });
-
-// get cosineTheta of two variables
-const cosineTheta = (a, b) =>
-  dot(a, b) / (Math.sqrt(dot(a, a)) * Math.sqrt(dot(b, b)));
-
-const magnitude = (vec) =>
-  Math.sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
-
-function isOpen(base, pip, dip, tip, wrist) {
-  const pipToTip = sub(tip, pip);
-  const wristToBase = sub(base, wrist);
-
-  const cosineTheta =
-    (dot(wristToBase, pipToTip) / magnitude(pipToTip)) * magnitude(wristToBase);
-
-  if (cosineTheta > 0.0) {
-    return true;
-  } else if (cosineTheta < 0.0) {
-    return false;
-  }
-}
-// determine the gesture of this loop
-const gesture = (
-  dist,
-  distThreshold,
-  indexOpen,
-  middleOpen,
-  ringOpen,
-  pinkyOpen
-) => {
-  // if index open and rest closed, return draw
-  if (indexOpen && !middleOpen && !ringOpen && !pinkyOpen) {
-    return "draw";
-  }
-  // else if index and middle open and rest closed, return erase
-  else if (indexOpen && middleOpen && ringOpen && !pinkyOpen) {
-    return "erase";
-  } else {
-    return "hover";
-  }
-  return "hover";
-};
-
-const mapRange = (value, min, max, newMin, newMax) => {
-  // map and clip
-  const valueClipped = Math.min(max, Math.max(min, value));
-  const out = ((valueClipped - min) / (max - min)) * (newMax - newMin) + newMin;
-
-  return out;
-};
-
-const strokeSize = (thumBase, indexBase, thumbTip) => {
-  const baseVec = sub(indexBase, thumBase);
-  const tipVec = sub(thumbTip, thumBase);
-
-  const cosT = cosineTheta(baseVec, tipVec);
-  const out = mapRange(cosT, 0.5, 1.0, 0.0, MAX_STROKE_WEIGHT);
-
-  return out;
-};
 
 export default function CameraCanvas() {
   const videoRef = useRef();
@@ -171,12 +42,6 @@ export default function CameraCanvas() {
         ctx.lineTo(points[i + 1].x, points[i + 1].y);
         ctx.stroke();
       }
-
-      // ctx.moveTo(points[0].x, points[0].y);
-      // for (let i = 1; i < points.length; i++) {
-      //     ctx.lineWidth = points[i].weight;
-      //     ctx.lineTo(points[i].x, points[i].y);
-      // }
     }
 
     function onResults(results) {
@@ -356,7 +221,9 @@ export default function CameraCanvas() {
   }, []);
 
   return (
+    <div>
     <div className="container">
+      
       <video ref={videoRef} className="input_video"></video>
       <canvas
         ref={canvasRef}
@@ -364,6 +231,8 @@ export default function CameraCanvas() {
         width="1280px"
         height="720px"
       ></canvas>
+    </div>
+
     </div>
   );
 }
